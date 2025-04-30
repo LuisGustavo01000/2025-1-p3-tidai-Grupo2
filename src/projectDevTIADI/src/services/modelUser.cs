019e1739d64f1c5bb9985810127db0f46810288f
AllanJunio <allanjuniomkt@gmail.com>
2025-04-30 20:53:11 -0300

    Atualização diretorio back

--- src/projectDevTIADI/src/services/modelUser.cs
+++
@@ -1,7 +0,0 @@
-    public class Login
-    {
-        public int Id { get; set; }
-        public int IdLogin { get; set; }
-        public string Localizacao { get; set; }
-        public int QuantidadeAtual { get; set; }
-    }
